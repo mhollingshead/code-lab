05a61e735fd5e5e30c89e4660e0e5708c1231aa8
Michael Hollingshead <michael.hollingshead@mail.mcgill.ca>
2021-07-23 03:27:37 -0400

    Fix editor lock bug when copying link to clipboard

--- scripts/index.js
+++ scripts/index.js
@@ -57,8 +57,9 @@ function init() {
         });
         document.querySelector('#code-id').addEventListener('click', () => {
             // Add temporary element to body
-            document.body.innerHTML += `<input value="${window.location.href}" id="tmp" />`;
-            const tmp = document.querySelector('#tmp');
+            const tmp = document.createElement('input');
+            tmp.value = window.location.href;
+            document.body.appendChild(tmp);
 
             // Select + copy it, then remove it
             tmp.select();
@@ -126,57 +127,57 @@ function getExampleRef() {
 }
 
 // Override default console.log and redirect logs to our pseudo console
-const legacy = console.log;
-(function () {
-    const logger = document.getElementById('log');
-    console.log = function () {
-        for (var i = 0; i < arguments.length; i++) {
-            if (typeof arguments[i] == 'object') {
-                // If its an object, stringify
-                logger.innerHTML += `
-                    <div class="op">
-                        <div class="carrot">
-                            <span class="m-i">
-                                navigate_before
-                            </span>
-                        </div>
-                        <pre class="output">${(JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i])}</pre>
-                    </div>
-                `;
-            } else {
-                // Else, log value
-                logger.innerHTML += `
-                    <div class="op">
-                        <div class="carrot">
-                            <span class="m-i">
-                                navigate_before
-                            </span>
-                        </div>
-                        <pre class="output">${arguments[i]}</pre>
-                    </div>
-                `;
-            }
-            // Scroll to bottom of the console on new logs
-            logger.scrollTop = logger.scrollHeight;
-        }
-    }
-})()
+// const legacy = console.log;
+// (function () {
+//     const logger = document.getElementById('log');
+//     console.log = function () {
+//         for (var i = 0; i < arguments.length; i++) {
+//             if (typeof arguments[i] == 'object') {
+//                 // If its an object, stringify
+//                 logger.innerHTML += `
+//                     <div class="op">
+//                         <div class="carrot">
+//                             <span class="m-i">
+//                                 navigate_before
+//                             </span>
+//                         </div>
+//                         <pre class="output">${(JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i])}</pre>
+//                     </div>
+//                 `;
+//             } else {
+//                 // Else, log value
+//                 logger.innerHTML += `
+//                     <div class="op">
+//                         <div class="carrot">
+//                             <span class="m-i">
+//                                 navigate_before
+//                             </span>
+//                         </div>
+//                         <pre class="output">${arguments[i]}</pre>
+//                     </div>
+//                 `;
+//             }
+//             // Scroll to bottom of the console on new logs
+//             logger.scrollTop = logger.scrollHeight;
+//         }
+//     }
+// })()
 
-// Log errors to console with necessary styling
-function logError(e) {
-    const logger = document.getElementById('log');
-    const themeClass = (theme === "dark") ? "dark" : "";
-    logger.innerHTML += `
-        <div class="op error ${themeClass}">
-            <div class="carrot error ${themeClass}">
-                <span class="m-i">
-                    error
-                </span>
-            </div>
-            <pre class="output error ${themeClass}">${e}</pre>
-        </div>
-    `;
-};
+// // Log errors to console with necessary styling
+// function logError(e) {
+//     const logger = document.getElementById('log');
+//     const themeClass = (theme === "dark") ? "dark" : "";
+//     logger.innerHTML += `
+//         <div class="op error ${themeClass}">
+//             <div class="carrot error ${themeClass}">
+//                 <span class="m-i">
+//                     error
+//                 </span>
+//             </div>
+//             <pre class="output error ${themeClass}">${e}</pre>
+//         </div>
+//     `;
+// };
 
 // Generate initial user IDs
 function makeId(length) {
